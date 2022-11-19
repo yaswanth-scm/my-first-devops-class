ec11951d74d61a1e8537154aca1e2e4e59d72d21
yaswanth <byaswanth207@gmail.com>
2022-11-19 05:23:29 +0000

    my first devops class

---
+++ home.java
@@ -0,0 +1,18 @@
+class.devops
+
+public static void main (string args[]){
+
+       system.out.println ("hai this is yaswanth");
+
+       system.out.println ("hai this is ramu");
+
+       system.out.println ("hai this is shesu");
+
+       system.out.println ("hai this is srinu");
+
+       system.out.println ("hai this is rajesh");
+
+       system.out.println ("hai this is ramesh");
+
+       system.out.println ("hai this is rakesh");
+
